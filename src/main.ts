c16bf80b696eb3eba38ffa9c6ce384bc07dbb2b4
pakhdev <t604977944@gmail.com>
2023-11-25 21:11:28 +0100

    Optimización de las fuentes y de los SVGs

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,12 @@ import { AppComponent } from './app/app.component';
 import { routes } from './app/routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { AuthorizationInterceptor } from './app/shared/interceptors/authorization.interceptor';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 bootstrapApplication(AppComponent, {
     providers: [
         provideRouter(routes),
+        provideAnimations(),
         provideHttpClient(
             withInterceptors([AuthorizationInterceptor]),
         ),
